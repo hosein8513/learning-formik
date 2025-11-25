a3f0adf86353b7552795d00c00966c6993bc3717
hosein <kazmyh279@gmail.com>
2025-11-25 11:05:55 +0330

    using-yup

--- src/Register.jsx
+++ src/Register.jsx
@@ -1,32 +1,46 @@
 import { useFormik } from "formik";
+import * as Yup from "yup"
+
+
+const initialValues= {
+            name: '',
+            email: '',
+            password: ''
+        }
+        const  onSubmit= value => {
+            console.log(value);
+
+        }
+        // const  validate = value => {
+        //     let errors = {}
+        //     if (!value.name) {
+        //         errors.name = "please fill this part"
+        //     }
+        //     if (!value.email) {
+        //         errors.email = "please fill this part"
+        //     } else if (!  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value.email)) {
+        //         errors.email = "please write an email"
+        //     }
+        //     if (!value.password) {
+        //         errors.password = "please fill this part"
+        //     }
+        //     return errors
+        // }
+        const validationSchema = Yup.object({
+            name:Yup.string().required("please fill this part"),
+            email:Yup.string().required("please fill this part").email("please write an email"),
+            password:Yup.string().required("please fill this part").min(7,"please enter atleast 7 chars")
+        })
+
+
 
 const Register = () => {
 
     const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            password: ''
-        },
-        onSubmit: value => {
-            console.log(value);
-
-        },
-        validate: value => {
-            let errors = {}
-            if (!value.name) {
-                errors.name = "please fill this part"
-            }
-            if (!value.email) {
-                errors.email = "please fill this part"
-            } else if (!  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(value.email)) {
-                errors.email = "please write an email"
-            }
-            if (!value.password) {
-                errors.password = "please fill this part"
-            }
-            return errors
-        }
+        initialValues,
+        onSubmit,
+        // validate
+        validationSchema
     })
     console.log(formik);
 
@@ -38,16 +52,16 @@ const Register = () => {
                 <form className="w-full h-full flex flex-col justify-center items-center gap-7 rounded-lg" onSubmit={formik.handleSubmit}>
                     <h1 className="text-[20px] font-bold">Sign In</h1>
                     <div className="flex flex-col justify-center items-center w-full">
-                        <input type="text" placeholder="name:" className="w-7/12 bg-black/40 rounded-lg p-2" name="name" value={formik.values.name} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                        <input type="text" placeholder="name:" className="w-7/12 bg-black/40 rounded-lg p-2" name="name" {...formik.getFieldProps('name')} />
                         {formik.errors.name && formik.touched.name ? <span className="text-[15px] text-center text-red-500">{formik.errors.name}</span> : ''}
                     </div>
                     <div className="flex flex-col justify-center items-center w-full">
-                        <input type="email" placeholder="email:" className="w-7/12 bg-black/40 rounded-lg p-2" name="email" value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} />
+                        <input type="email" placeholder="email:" className="w-7/12 bg-black/40 rounded-lg p-2" name="email"{...formik.getFieldProps('email')} />
                         {formik.errors.email  ? <span className="text-[15px] text-center text-red-500">{formik.errors.email}</span> : ''}
                     </div>
                     <div className="flex flex-col justify-center items-center w-full">
-                        <input type="password" placeholder="password:" className="w-7/12 bg-black/40 rounded-lg p-2" name="password" value={formik.values.password} onChange={formik.handleChange} onBlur={formik.handleBlur} />
-                        {formik.errors.password && formik.touched.password ? <span className="text-[15px] text-center text-red-500">{formik.errors.password}</span> : ''}
+                        <input type="password" placeholder="password:" className="w-7/12 bg-black/40 rounded-lg p-2" name="password" {...formik.getFieldProps('password')} />
+                        {formik.errors.password ? <span className="text-[15px] text-center text-red-500">{formik.errors.password}</span> : ''}
                     </div>
                     <button className="w-[90px] h-[45px] rounded-lg text-white bg-green-500" type="submit">Submit</button>
                 </form>
